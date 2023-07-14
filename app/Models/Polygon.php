b5db6a25195c02bd4e6b5ae96e135191e40a8c92
Tiago de Castro Lima <casmei@protonmail.com>
2023-07-14 15:19:58 -0300

    ✨ define explicitamente o nome da tabela no banco

--- app/Models/Polygon.php
+++ app/Models/Polygon.php
@@ -8,10 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Polygon extends Model
 {
     use HasFactory;
+    protected $table = 'polygons';
 
-    protected $fillable = [
-        'base',
-        'height',
-        'type'
-    ];
+    protected $fillable = ['base', 'height', 'type'];
 }
