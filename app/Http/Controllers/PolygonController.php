fdc306c566e4c19f7178637c29f55d221870ef99
Tiago de Castro Lima <casmei@protonmail.com>
2023-07-14 00:24:03 -0300

    ✨ polygon controller and inject polygon service

---
+++ app/Http/Controllers/PolygonController.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Services\PolygonService;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class PolygonController extends Controller
+{
+    protected $polygonService;
+
+    public function __construct(PolygonService $polygonService)
+    {
+        $this->polygonService = $polygonService;
+    }
+
+    public function createRectangle(Request $request)
+    {
+        $base = $request->input('base');
+        $height = $request->input('height');
+
+        $rectangle = $this->polygonService->createRectangle($base, $height);
+
+        return response()->json($rectangle, 201);
+    }
+
+    public function createTriangle(Request $request)
+    {
+        $base = $request->input('base');
+        $height = $request->input('height');
+
+        $triangle = $this->polygonService->createTriangle($base, $height);
+
+        return response()->json($triangle, 201);
+    }
+
+    public function calculateTotalArea()
+    {
+        $totalArea = $this->polygonService->calculateTotalArea();
+
+        return response()->json(['total_area' => $totalArea]);
+    }
+}
