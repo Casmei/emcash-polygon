a57fd49c6e3895fbfcc910a6395400a22eb9cf12
Tiago de Castro Lima <casmei@protonmail.com>
2023-07-16 00:51:31 -0300

    ✨ reimplement totalArea method

--- app/Services/PolygonService.php
+++ app/Services/PolygonService.php
@@ -2,59 +2,22 @@
 
 namespace App\Services;
 
-use App\Dtos\CreatePolygonDto;
-use App\Enums\PolygonType;
-use App\Models\Polygon;
 use App\Models\Rectangle;
 use App\Models\Triangle;
 
 class PolygonService
 {
-    public function createRectangle(CreatePolygonDto $dto): Rectangle
-    {
-        return Rectangle::create([
-            'base' => $dto->base,
-            'height' => $dto->height
-        ]);
-    }
-
-    public function createTriangle(CreatePolygonDto $dto): Triangle
-    {
-        return Triangle::create([
-            'base' => $dto->base,
-            'height' => $dto->height
-        ]);
-    }
-
     public function calculateTotalArea(): float
     {
-        $polygons = Polygon::all();
+        $rectangles = Rectangle::all();
+        $triangles = Triangle::all();
+        $polygons = $rectangles->concat($triangles);
         $totalArea = 0;
 
         foreach ($polygons as $polygon) {
-            $polygonInstance = $this->createPolygonInstance($polygon);
-            if ($polygonInstance) {
-                $totalArea += $polygonInstance->calculateArea();
-            }
+            $totalArea += $polygon->calculateArea();
         }
 
         return $totalArea;
-    }
-
-    private function createPolygonInstance(Polygon $polygon): ?Polygon
-    {
-        $polygonData = [
-            'base' => $polygon->base,
-            'height' => $polygon->height,
-        ];
-
-        switch ($polygon->type) {
-            case PolygonType::RECTANGLE:
-                return new Rectangle($polygonData);
-            case PolygonType::TRIANGLE:
-                return new Triangle($polygonData);
-            default:
-                return null;
-        }
     }
 }
