3fa12edc0bc0ce2b68f729724bf0cb76e464932a
Tiago de Castro Lima <casmei@protonmail.com>
2023-07-17 00:05:20 -0300

    🐛  wait bd service up

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 cd /app
+
+until nc -z db 3306; do
+  echo "Aguardando o serviço db ficar disponível..."
+  sleep 1
+done
+
 php artisan key:generate
 php artisan migrate
 php artisan serve --host=0.0.0.0
