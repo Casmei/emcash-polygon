cf040f8301207d2cb10aab301bbdc884c5c944fb
Tiago de Castro Lima <casmei@protonmail.com>
2024-04-23 23:56:10 -0300

    feat: get db host env on entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 
 cd /app
 
-until nc -z db 3306; do
-  echo "Aguardando o serviço db ficar disponível..."
+until nc -z $DB_HOST 3306; do
+  echo "Aguardando o serviço de banco de dados ficar disponível..."
   sleep 1
 done
 
 php artisan key:generate
 php artisan migrate
-php artisan serve --host=0.0.0.0
+php artisan serve
